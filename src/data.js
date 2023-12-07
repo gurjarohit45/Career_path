45f9afd408e74d222d6e351c06bc0552d94e21e3
Rohit <gujjarohit450@gmail.com>
2023-12-07 21:22:45 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,59 @@
+const data = [
+   
+    {
+        id: 1,
+        name: "Unacademy",
+        info: " Unacademy Plus is a premium subscription that contains all the features like live classes, doubt sessions, live quizzes, etc.Unacademy operates on a freemium model, where many of the educational resources are available for free, but there are also subscription-based models for more comprehensive and in-depth courses.",
+        image: "https://play-lh.googleusercontent.com/DT0jm91vA_rOkxwatXhI4yVpjU4KI0augje6ZJGYxEyNjKvIhcQt8npiLHf5-ydclsqe",
+        price: "62,000",
+    },
+
+    {
+        id: 2,
+        name: "Physics Walah",
+        info: " Physics Wallah is a popular term used in India to refer to Mr. Alakh Pandey, an educator known for his online physics lectures and teaching videos.Physics Wallah's approach to education, providing high-quality content for free, has made a significant impact on the lives of many students across India, especially those who do not have access to expensive coaching institutes.",
+        image: "https://play-lh.googleusercontent.com/urT7EtJZ8TPhV1QBMGR1dpj_Eqzjxv7flEnCRAZTPmvpNMz4o06pn_m585rhN6gRNdY" ,
+        price: "13,000",
+    },
+    {
+        id: 3,
+        name: "Gravity",
+        info:  " As of my last update in September 2021, Gravity does not appear to be a well-known coaching institute for JEE (Joint Entrance Examination) preparation.The institute's main objective is to provide specialized coaching and guidance to students aspiring to crack the Joint Entrance Examination (JEE Main and JEE Advanced) for admission to the prestigious Indian Institutes of Technology (IITs).Over the years, FIITJEE has grown and expanded its coaching pro",
+        image: "https://storage.googleapis.com/production-bluehost-v1-0-4/974/1277974/caBSK5qq/da1c03ffac7a4345895b047baa479593",
+        price: "75,000",
+    },
+    
+   
+
+    {
+        id: 4,
+        name: "Vedantu",
+        info: " Vedantu's approach to education involves connecting students with experienced teachers in real-time through their virtual learning environment.Vedantu's key feature is its live online tutoring system, where students can attend live classes conducted by experienced teachers in real-time.",
+        image: " https://www.educationworld.in/wp-content/uploads/2019/02/vedantu.jpg",
+        price: "80,000",
+    },
+    {
+        id: 5,
+        name: "Allen Career Institute",
+        info: "A Premier Coaching Institute for the preparation of JEE (Main+Advanced).Allen Career Institute has gained a strong reputation for its quality teaching, expert faculty, comprehensive study material, and consistent success in producing top-ranked students in these competitive exams.  ",
+        image: " https://bl-i.thgim.com/public/incoming/ky0mvh/article65526359.ece/alternates/FREE_1200/allen-logo.jpg",        
+        price: "130,000",
+    },
+
+    {
+        id: 6,
+        name: "Aakash",
+        info: " India's Best Coaching for NEET (UG), IIT JEE Main, JEE Advanced, Class 6 to 12, NTSE, KVPY Olympiad & Other Competitive Exams.Joint Entrance Examination (JEE): JEE Main and JEE Advanced for admission to Indian Institutes of Technology (IITs) and other engineering colleges.",
+        image: " https://static.ambitionbox.com/assets/v2/images/rs:fit:1280:960:false:false/bG9jYWw6Ly8vbG9nb3Mvb3JpZ2luYWxzL2Fha2FzaC1pbnN0aXR1dGUuanBn.png" ,
+        price: "100,000",
+    },
+    {
+        id: 7,
+        name: "FIITJEE",
+        info: " FIITJEE (Forum for Indian Institute of Technology and Joint Entrance Examination) is one of India's leading coaching institutes that prepares students.Over the years, FIITJEE has grown and expanded its coaching programs to cover other exams like the All India Engineering Entrance Examination (AIEEE, now known as JEE Main), National Talent Search Examination (NTSE), Kishore Vaigyanik Protsahan Yojana (KVPY), and various Olympiads. ",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMPw0D78M3Tv9iA1uPOTE_kwyGRMS24NKpmw&usqp=CAU" ,
+        price: "110,000",
+    },
+
+];
+export default data;
